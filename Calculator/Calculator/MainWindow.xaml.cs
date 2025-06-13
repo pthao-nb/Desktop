31b83799d097308e658e8219b8b551f83f202108
pthao-nb <p.thao.221205@gmail.com>
2025-06-13 23:53:31 +0700

    Add files via upload

---
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            int num1 = Convert.ToInt32(txtOp1.Text);
+            int num2 = Convert.ToInt32(txtOp2.Text);
+            int answer = num1 + num2;
+            txtAns.Text = answer.ToString();
+        }
+
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            int num1 = Convert.ToInt32(txtOp1.Text);
+            int num2 = Convert.ToInt32(txtOp2.Text);
+            int answer = num1 - num2;
+            txtAns.Text = answer.ToString();
+        }
+
+        private void chkCenter_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if ((bool)chkCenter.IsChecked)
+            {
+                txtOp1.TextAlignment = TextAlignment.Center;
+                txtOp2.TextAlignment = TextAlignment.Center;
+                txtAns.TextAlignment = TextAlignment.Center;
+            }
+            else
+            {
+                txtOp1.TextAlignment = TextAlignment.Left;
+                txtOp2.TextAlignment = TextAlignment.Left;
+                txtAns.TextAlignment = TextAlignment.Left;
+            }
+        }
+    }
+}
---
+++ Data Binding/Data Binding/NumberOfPlayers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Data_Binding
+{
+    public class NumberOfPlayers : ObservableCollection<int>
+    {
+        public NumberOfPlayers()
+        {
+            Add(2);
+            Add(3);
+            Add(4);
+        }
+    }
+}
