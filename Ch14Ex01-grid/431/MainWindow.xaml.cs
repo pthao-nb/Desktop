1d0d6eefca5591519b2c84266db778c8db360bb8
pthao-nb <p.thao.221205@gmail.com>
2025-06-14 00:06:23 +0700

    Add files via upload

---
+++ Ch14Ex01-grid/431/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _431
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Window handler, bubbling up");
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Window handler, tunneling down");
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Grid handler, bubbling up");
+        }
+
+        private void Grid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Grid handler, tunneling down");
+            // e.Handled = true; // Bỏ comment nếu muốn chặn sự kiện lan lên
+        }
+
+        private void rotatedButton_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("rotatedButton handler, bubbling up");
+        }
+
+        private void rotatedButton_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("rotatedButton handler, tunneling down");
+        }
+
+    }
+}
